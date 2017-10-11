6573aa4cee2472212d53ce22a8709d1480d1f226
Robin van der Vleuten <robin@webstronauts.co>
2017-10-11 09:28:43 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+export default {
+  parser: require.resolve('babel-eslint'),
+  extends: 'airbnb',
+  rules: {
+    'array-bracket-spacing': [ 'error', 'always' ],
+    'arrow-parens': [ 'error', 'as-needed' ],
+    'comma-dangle': [ 'error', {
+      arrays: 'only-multiline', objects: 'only-multiline', imports: 'never', exports: 'never', functions: 'ignore'
+    } ],
+    'react/jsx-filename-extension': [ 'error', {
+      extensions: [ '.js', '.jsx' ]
+    } ],
+    'react/prop-types': 'off'
+  }
+};
