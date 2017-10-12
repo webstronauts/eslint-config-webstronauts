714fa514c4aab04eb5d5a698515fe2f335062238
Robin van der Vleuten <robin@webstronauts.co>
2017-10-12 10:35:09 +0200

    Disallow bracket space for object in array

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ export default {
   parser: require.resolve('babel-eslint'),
   extends: 'airbnb',
   rules: {
-    'array-bracket-spacing': [ 'error', 'always' ],
+    'array-bracket-spacing': [ 'error', 'always', { objectsInArrays: false }],
     'arrow-parens': [ 'error', 'as-needed' ],
     'comma-dangle': [ 'error', {
       arrays: 'only-multiline', objects: 'only-multiline', imports: 'never', exports: 'never', functions: 'ignore'
-    } ],
+    }],
     'react/jsx-filename-extension': [ 'error', {
       extensions: [ '.js', '.jsx' ]
-    } ],
+    }],
     'react/prop-types': 'off'
   }
 };
